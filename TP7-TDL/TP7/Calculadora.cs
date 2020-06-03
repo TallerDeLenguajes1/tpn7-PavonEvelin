a6c0a028f37471d59a988b280a42bcbe398bf073
PavonEvelin <evelinluz17@gmail.com>
2020-06-03 18:30:30 -0300

    no se

---
+++ TP7-TDL/TP7/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP7
+{
+    class Calculadora
+    {
+        private float numero1f, numero2f;
+
+        public float Numero1f { get => numero1f; set => numero1f = value; }
+        public float Numero2f { get => numero2f; set => numero2f = value; }
+
+        public float Suma()
+        {
+            return Numero1f + Numero2f;
+        }
+
+        public float Resta()
+        {
+            return Numero1f - Numero2f;
+        }
+
+        public float Multiplicacion()
+        {
+            return Numero1f * Numero2f;
+        }
+
+        public float Division()
+        {
+            if (Numero2f != 0.0f)
+            {
+                return Numero1f / Numero2f;
+            }
+            else
+            {
+                Console.WriteLine("Error. El divisor debe ser mayor a 0\n");
+            }
+            
+        }
+    }
+}
